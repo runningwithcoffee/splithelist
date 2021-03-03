34776ddea88c835bf32129e382d3238471a43628
Emir <ecanduysak@hotmail.com>
2021-03-03 23:56:01 +0300

    surname correction

--- src/main/java/split.java
+++ src/main/java/split.java
@@ -5,34 +5,47 @@ import java.util.Scanner;
 public class split {
     private Sheet sheet;
     private XSSFWorkbook wb;
+    public String capitalize(String str) {
+        if(str == null || str.isEmpty()) {
+            return str;
+        }
+
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
     public void savetofile() throws IOException
     {
-        FileOutputStream outFile =new FileOutputStream(new File("update.xlsx"));
+        FileOutputStream outFile = new FileOutputStream("update.xlsx");
         wb.write(outFile);
         outFile.close();
     }
     public void gettowork() throws IOException{
-        int columncount=0;
-        int rowcount=0;
-        String[] sınıflar = {"A","B","C","D","E","F","G","H","I","J","K","L","M","O","P","R","S","T","U","V","Y","Z","X"};
+        //int columncount=0;
+        int maxroom=0,dv=0;
+        String[] classarr = {"A","B","C","D","E","F","G","H","I","J","K","L","M","O","P","R","S","T","U","V","Y","Z","X"};
         Scanner in = new Scanner(System.in);
-        System.out.print("kaçlı gruplara bölmek istiyorsun: ");
-        int dv = in.nextInt();
-        if (dv<15 || dv>30)
+        do
         {
-            System.out.print("HATA! en az 15 kişi, en fazla 30 kişi yazman gerek:");
+            System.out.print("grupları hangi sayıya bölmek istiyorsun? (en az 10 en fazla 30): ");
             dv = in.nextInt();
-        }
-        FileInputStream inputStream = new FileInputStream(new File("tablo1.xlsx"));
+
+        } while (dv<10 || dv>30);
+        do
+        {
+            System.out.print("bloklardaki max oda sayısı kaç olucak? (en fazla 999): ");
+            maxroom = in.nextInt();
+            maxroom++;
+
+        } while (maxroom==0 || maxroom>1000 || maxroom<30);
+        FileInputStream inputStream = new FileInputStream("tablo1.xlsx");
         wb = new XSSFWorkbook(inputStream);
         sheet = wb.getSheetAt(0);
-        rowcount = sheet.getLastRowNum();
+        int rowcount = sheet.getLastRowNum();
         //Cell cell = sheet.getRow(1).createCell(6);
         //cell.setCellValue("TEST");
 
-        int i=2;
-        int sınıfsırası=0;
-        int odasırası=1;
+        int i=1;
+        int classnumber=0;
+        int roomnumber=1;
         while (i<rowcount)
         {
 
@@ -42,7 +55,13 @@ public class split {
              if (surname==null)
              {
               surname=sheet.getRow(i).createCell(2);
-              //TODO emailden soyadını düzelme
+              String temp = sheet.getRow(i).getCell(14).getStringCellValue();
+                 int index = temp.indexOf('@');
+                 temp = temp.substring(0,index);
+                 temp = temp.split("\\.")[1];
+                 temp = temp.replaceAll("[0-9]", "");
+                 temp = capitalize(temp);
+                 surname.setCellValue(temp);
               //TODO olabiliyorsa türkçe düzeltme
 
              }
@@ -51,22 +70,22 @@ public class split {
                  username=sheet.getRow(i).createCell(3);
                  String temp;
                  temp = sheet.getRow(i).getCell(14).getStringCellValue();
-                 System.out.println(temp);
+                 //System.out.println(temp);
                  int index = temp.indexOf('@');
                  temp = temp.substring(0,index);
                  temp = temp.replace(".","");
                  username.setCellValue(temp);
              }
-             String val = sınıflar[sınıfsırası]+""+odasırası;
+             String val = classarr[classnumber]+""+roomnumber;
              cell.setCellValue(val);
             if((i%dv)==0)
             {
-                odasırası++;
+                roomnumber++;
             }
-            if ((odasırası % 51)==0)
+            if ((roomnumber % maxroom)==0)
             {
-                sınıfsırası++;
-                odasırası=1;
+                classnumber++;
+                roomnumber=1;
             }
             i++;
             //TODO son satıra sınıf atamıyor
