f15801c8c9330aa2bcca0b7873720eb105382858
Emir <ecanduysak@hotmail.com>
2021-03-08 00:59:26 +0300

    v2

--- src/main/java/split.java
+++ src/main/java/split.java
@@ -3,7 +3,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.*;
 import java.util.Scanner;
 public class split {
-    private Sheet sheet;
+    private static String filename;
     private XSSFWorkbook wb;
     public String capitalize(String str) {
         if(str == null || str.isEmpty()) {
@@ -12,50 +12,47 @@ public class split {
 
         return str.substring(0, 1).toUpperCase() + str.substring(1);
     }
+    //TODO autodetect excel files example: .xlsx add to list
     public void savetofile() throws IOException
     {
-        FileOutputStream outFile = new FileOutputStream("update.xlsx");
+        File theDir = new File("output");
+        if (!theDir.exists()){
+            theDir.mkdirs();
+        }
+        String path="";
+        FileOutputStream outFile = new FileOutputStream("output/"+filename+"_fixed.xlsx");
         wb.write(outFile);
         outFile.close();
     }
     public void gettowork() throws IOException{
-        //int columncount=0;
-        int maxroom=0,dv=0;
-        String[] classarr = {"A","B","C","D","E","F","G","H","I","J","K","L","M","O","P","R","S","T","U","V","Y","Z","X"};
         Scanner in = new Scanner(System.in);
-        do
-        {
-            System.out.print("grupları hangi sayıya bölmek istiyorsun? (en az 10 en fazla 30): ");
-            dv = in.nextInt();
-
-        } while (dv<10 || dv>30);
-        do
-        {
-            System.out.print("bloklardaki max oda sayısı kaç olucak? (en fazla 999): ");
-            maxroom = in.nextInt();
-            maxroom++;
-
-        } while (maxroom==0 || maxroom>1000 || maxroom<30);
-        FileInputStream inputStream = new FileInputStream("tablo1.xlsx");
+        System.out.print("dosya ismi?:");
+        filename = in.nextLine();
+        FileInputStream inputStream = new FileInputStream(filename+".xlsx");
         wb = new XSSFWorkbook(inputStream);
-        sheet = wb.getSheetAt(0);
+        Sheet sheet = wb.getSheetAt(0);
         int rowcount = sheet.getLastRowNum();
-        //Cell cell = sheet.getRow(1).createCell(6);
-        //cell.setCellValue("TEST");
-
         int i=1;
-        int classnumber=0;
-        int roomnumber=1;
-        while (i<rowcount)
+        while (i<=rowcount)
         {
 
-             Cell cell = sheet.getRow(i).createCell(6);
-             Cell username=sheet.getRow(i).getCell(3);
+             Cell name= sheet.getRow(i).getCell(1);
+             Cell username= sheet.getRow(i).getCell(3);
              Cell surname= sheet.getRow(i).getCell(2);
-             if (surname==null)
+             Cell pass= sheet.getRow(i).getCell(4);
+             Cell room=sheet.getRow(i).getCell(6);
+             Cell emailadress = sheet.getRow(i).getCell(14);
+             String tr = room.getStringCellValue();
+             int rlength = tr.length();
+             String emailtemp = emailadress.getStringCellValue();
+
+
+
+            /* if (surname==null && name==null) //name & surname from mail adress
              {
-              surname=sheet.getRow(i).createCell(2);
-              String temp = sheet.getRow(i).getCell(14).getStringCellValue();
+                 name=sheet.getRow(i).createCell(1);
+                surname=sheet.getRow(i).createCell(2);
+                String temp = sheet.getRow(i).getCell(14).getStringCellValue();
                  int index = temp.indexOf('@');
                  temp = temp.substring(0,index);
                  temp = temp.split("\\.")[1];
@@ -65,30 +62,85 @@ public class split {
               //TODO olabiliyorsa türkçe düzeltme
 
              }
-             if (username==null)
+             */
+            if ((room != null || room.getStringCellValue() != "") && rlength<3 )
+            {
+                String troom = room.getStringCellValue();
+                char c1,c2;
+                c1 = troom.charAt(0);
+                c2 = troom.charAt(1);
+                troom = c1+"-"+c2;
+                troom.trim();
+                room.setCellValue(troom);
+            }
+             if (username == null || username.getStringCellValue() == "")
              {
-                 username=sheet.getRow(i).createCell(3);
-                 String temp;
-                 temp = sheet.getRow(i).getCell(14).getStringCellValue();
-                 //System.out.println(temp);
-                 int index = temp.indexOf('@');
-                 temp = temp.substring(0,index);
-                 temp = temp.replace(".","");
-                 username.setCellValue(temp);
+                 if (emailadress==null)
+                 {
+                     System.out.println("Email adresi boş hücre:0"+i);
+                     break;
+                 }
+                 else
+                     {
+                     username = sheet.getRow(i).createCell(3);
+                     int index = emailtemp.indexOf('@');
+                     emailtemp = emailtemp.substring(0, index);
+                     emailtemp = emailtemp.replace(".", "");
+                     username.setCellValue(emailtemp);
+                 }
              }
-             String val = classarr[classnumber]+""+roomnumber;
-             cell.setCellValue(val);
-            if((i%dv)==0)
-            {
-                roomnumber++;
-            }
-            if ((roomnumber % maxroom)==0)
-            {
-                classnumber++;
-                roomnumber=1;
-            }
+             if (surname == null || surname.getStringCellValue() == "")
+             {
+              surname = sheet.getRow(i).createCell(2);
+              name = sheet.getRow(i).getCell(1);
+              String surtemp = name.getStringCellValue();
+              String[] surarry = surtemp.split(" ");
+              String fname = "";
+              surtemp = surarry[surarry.length-1];
+              int b = surarry.length-1;
+              for (int z=0;z<b;z++)
+                 {
+                     fname += " "+ surarry[z];
+                 }
+              surtemp=surtemp.trim();
+              surname.setCellValue(surtemp);
+              fname=fname.trim();
+              name.setCellValue(fname);
+             }
+             //final trim
+            name= sheet.getRow(i).getCell(1);
+            username= sheet.getRow(i).getCell(3);
+            surname= sheet.getRow(i).getCell(2);
+            pass= sheet.getRow(i).getCell(4);
+            room=sheet.getRow(i).getCell(6);
+            emailadress = sheet.getRow(i).getCell(14);
+            String fnametrim = "";
+            String fsurnametrim = "";
+            String fusernametrim = "";
+            String froomtrim = "";
+            String femailtrim = "";
+
+
+             fnametrim = name.getStringCellValue();
+             fsurnametrim = surname.getStringCellValue();
+             fusernametrim = username.getStringCellValue();
+             froomtrim = room.getStringCellValue();
+             femailtrim = emailadress.getStringCellValue();
+             fnametrim.trim();
+             fsurnametrim.trim();
+             fusernametrim.trim();
+             froomtrim.trim();
+             femailtrim.trim();
+             name.setCellValue(fnametrim);
+             surname.setCellValue(fsurnametrim);
+             username.setCellValue(fusernametrim);
+             room.setCellValue(froomtrim);
+             emailadress.setCellValue(femailtrim);
+             pass= sheet.getRow(i).createCell(4);
+             pass.setCellValue(123456);
+
+
             i++;
-            //TODO son satıra sınıf atamıyor
         }
         savetofile();
 
